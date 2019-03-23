afb2c6459e5b9934a56cdd9c8e067016644f7ede
kwon <hdragon1501@gmail.com>
2019-03-23 11:54:50 +0700

    update database

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const bcrypt  		= require('bcrypt-nodejs');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-acute-66978',
-    user : 'postgres',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl:true
   }
 });
 
